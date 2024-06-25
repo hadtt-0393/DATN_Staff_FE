a07423c83ff9e1e033c7aac8a92b29d6f568b627
dinh.thi.thu.ha <dinh.thi.thu.ha@sun-asterisk.com>
2024-06-25 16:04:22 +0700

    update

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -6,13 +6,17 @@ const convertDate = (date: string) => {
     return new Date(date).toLocaleDateString('en-GB');
 }
 
-interface Rooms {
+interface RoomChooseName {
     roomName: string;
     quantity: number;
 }
 
-const convertRoomToString = (rooms: Rooms[]) => {
+const convertRoomToString = (rooms: RoomChooseName[]) => {
     return rooms.map(room => (`${room.quantity} x ${room.roomName}`)).join('')
 }
 
-export { convertPrice, convertDate, convertRoomToString }
+const convertTime = (date: string) => {
+    return new Date(date).toLocaleString('en-GB');
+}
+
+export { convertPrice, convertDate, convertRoomToString, convertTime }
